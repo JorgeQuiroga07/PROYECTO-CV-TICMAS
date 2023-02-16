074a95076009efd8c6a7ace7ad614784b8aaa7f2
Jorge Quiroga <quirogajorge895@gmail.com>
2023-02-16 00:09:46 -0300

    Agregado de boton ir arriba

--- javascript.js
+++ javascript.js
@@ -20,3 +20,24 @@ btnAbrirModal.addEventListener("click",()=>{
 btnCerrarModal.addEventListener("click",()=>{
     modal.close();
 })
+
+
+const btn_scrolltop = document.getElementById("btn_scrolltop")
+btn_scrolltop.addEventListener("click", () => {
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+})
+
+window.onscroll = () => {
+    add_btn_scrolltop()
+}
+
+const add_btn_scrolltop = () => {
+    if (window.scrollY < 3600) {
+        btn_scrolltop.classList.remove("btn-scrolltop-on")
+    } else {
+        btn_scrolltop.classList.add("btn-scrolltop-on")   
+    }
+}
