de513e921cf0841d8cc42bae58c7ab68af43d237
Jorge Quiroga <quirogajorge895@gmail.com>
2023-02-26 13:59:26 -0300

    Modificacion imagen,color de letra y botones

--- javascript.js
+++ javascript.js
@@ -3,10 +3,12 @@ const button = document.querySelector('.saludo');
 button.addEventListener("click", saludar);
 
 function saludar(){
-    alert("Bienvenida a mi web! Disfruta el recorrido ☺️");
+    /*swal(por alert)*/alert("Bienvenida a mi web! Disfruta el recorrido ⚡️ ");
     /*button.removeEventListener("click", saludar);*/
 } 
-
+/*
+☺️
+*/
 const btnAbrirModal =
 document.querySelector("#btn-abrir-modal");
 const btnCerrarModal =
